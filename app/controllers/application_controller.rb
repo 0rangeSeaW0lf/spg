2b66c7441539adde52f9905ca854d31072a69fbe
José Moreno <jose.moreno@reyesic.net>
2016-12-03 16:48:20 +0000

    Minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-      @today = Date.today
-      @early_bird = Date.new(2016,12,31)
+      @end_registration = DateTime.new(2017,03,18,00,00,00)
+      @today = Time.now
+      @early_bird = DateTime.new(2016,12,31,00,00,00)
       render :template => 'index'
   end
 end
