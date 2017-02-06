3ef61108091cba7f5526318bd0b8f3ee331f196d
José Moreno <jose.moreno@reyesic.net>
2017-02-06 22:02:59 +0000

    Update registration closing date

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-      @end_registration = DateTime.new(2017,03,18,00,00,00)
+      @end_registration = DateTime.new(2017,03,13,00,00,00)
       @today = Time.now
       @early_bird = DateTime.new(2016,12,31,00,00,00)
       render :template => 'index'
